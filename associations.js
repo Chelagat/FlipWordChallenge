714f4130d3c6b8c957d57abe22a120bb56340990
Norah Borus <nborus@box.com>
2017-08-21 01:33:37 -0700

    Add timer

--- associations.js
+++ associations.js
@@ -5,19 +5,50 @@ BUTTONS = new Array();
 OFF_COLOR = 'gray';
 ON_COLOR = 'gold';
 DEFAULT_NUM = 4;
-
+DURATION = 10;
+PLAYER_NAME = "Stranger";
+let timerElem = document.getElementById('timer');
 function initialize(num_buttons){
-    input = document.getElementById("numButtons").value;
-    NUM_BUTTONS = input == ""? DEFAULT_NUM: parseInt(input);
+    buttonsInput = document.getElementById("numButtons").value;
+    PLAYER_NAME = document.getElementById("playerName").value;
+    NUM_BUTTONS = buttonsInput == ""? DEFAULT_NUM: parseInt(buttonsInput);
     NUM_BUTTONS = NUM_BUTTONS < 1? DEFAULT_NUM: NUM_BUTTONS;
     HORIZONTAL_FACTOR = Math.floor(Math.sqrt(NUM_BUTTONS));
     console.log('HORIZONTAL_FACTOR', HORIZONTAL_FACTOR);
     document.getElementById('introduction').classList.add('hidden');
     document.getElementsByClassName('button-area')[0].classList.remove('hidden');
     initializeButtons();
+    timeLeft = DURATION;
+    timerId = setInterval(countDown,1000);
 
 }
 
+function countDown() {
+  if (timeLeft == 0) {
+    endGame(false);
+  } else {
+    timerElem.innerHTML = timeLeft + ' seconds remaining';
+    timeLeft--;
+  }
+}
+
+function endGame(playerWon){
+  clearTimeout(timerId);
+  timerElem.classList.add('hidden');
+  document.getElementsByClassName("button-area")[0].classList.add('hidden');
+  if (playerWon){
+    let audio = new Audio('ta-da.mp3');
+    audio.play();
+    document.getElementById("message").classList.remove('hidden');
+    document.getElementById("win").innerHTML = "Good job, "+PLAYER_NAME+"!";
+  }else{
+    let audio = new Audio('you-lose.mp3');
+    audio.play();
+    document.getElementById("message").classList.remove('hidden');
+    document.getElementById("lose").innerHTML = "Better luck next time, "+PLAYER_NAME+"!";
+  }
+
+}
 function initializeButtons(){
     for (i=0; i < NUM_BUTTONS;i++){
         let button = document.createElement("button");
@@ -48,10 +79,7 @@ function flipState(event){
      flipColor(targetElement);
      flipColor(partnerElement);
      if (gameWon()){
-         let audio = new Audio('ta-da.mp3');
-         audio.play();
-         document.getElementsByClassName("button-area")[0].classList.add('hidden');
-         document.getElementById("message").classList.remove('hidden');
+         endGame(true);
      }
 }
 
